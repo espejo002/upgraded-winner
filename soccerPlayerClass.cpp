2be839f1860c64b7da82e11c1eae96b254c75924
espejo002 <iseyemijoshua002@gmail.com>
2024-01-02 22:00:23 +0100

    Create soccerPlayerClass.cpp

---
+++ soccerPlayerClass.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Player {
+      public:
+             string name;
+             int age;
+             string nationality;
+             string club;
+             int height;
+             int clubShirtNumber;
+             int nationShirtNumber;
+             char dominantFoot;
+}
