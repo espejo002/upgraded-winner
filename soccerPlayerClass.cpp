bbbb788aac876ceca2e3417f57f4c2a215ce87d9
espejo002 <iseyemijoshua002@gmail.com>
2024-01-03 22:02:28 +0100

    Update soccerPlayerClass.cpp
    A soccer player with exquisite qualities and rules for developing soccer players

--- soccerPlayerClass.cpp
+++ soccerPlayerClass.cpp
@@ -11,4 +11,23 @@ class Player {
              int clubShirtNumber;
              int nationShirtNumber;
              char dominantFoot;
+             Player(string Name, int Age, string Nationality, string Club, int Height, int CSN, int NSN, char DominantFoot) {
+                   name = Name;
+                   setRules(Age);
+                   nationality = Nationality;
+                   club = Club;
+                   height = Height;
+
+
+
+             void ageRules(int aAge) {
+                   if(Age >= 15 && Age <= 45) {
+                         age = Age;
+                   } else {
+                         cout << "The age is not valid." << endl;
+                         cout << "It must be between 15 and 45" << endl;
+                   }
+             }
+             };
+
 }
